52f49e8907d114b3c3cf8a9db20161324e5ad7ab
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-03-04 15:32:16 -0600

    Added ability to create table rows based on user input

--- client.js
+++ client.js
@@ -17,12 +17,20 @@ function readyNow(){
 
 function addEmployee() {
     console.log('CLICK');
+    let employee = {
+        firstName: $('#firstNameInput').val(),
+        lastName: $('#lastNameInput').val(),
+        id: $('#idInput').val(),
+        title: $('#titleInput').val(),
+        salary: $('#salaryInput').val()
+    };
     $('#newEmployee').append(`<tr> 
         <td>${employee.firstName}</td>
         <td>${employee.lastName}</td>
         <td>${employee.id}</td>
         <td>${employee.title}</td>
         <td>${employee.salary}</td>
+        <td><button>Delete</button></td>
     </tr>
     `)
     
